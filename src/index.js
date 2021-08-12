db7a9a2ce3410ce54e780e113c1e10d3c622ecfb
sagarva <sagarvakumar@gmail.com>
2021-08-12 17:01:54 +0530

    combineReducers Application

--- src/index.js
+++ src/index.js
@@ -2,43 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore } from 'redux';  //We can use combineReducer in order to add multiple reducer in a single store
+import allReducers from './Reducers/index';
 
 
-//Store well the store is a place to store data as 
+const store = createStore(allReducers)
 
-//action
-const increment =()=> {
-  return{
-    type:"INCREMENT"
-  }
-}
-const decrement =()=> {
-  return{
-    type:"DECREMENT"
-  }
-}
-//reducer will tell us what is supposed to happen when we perform an action
-
-const counter =( state=0, action) => {
-  switch(action.type){
-    case "INCREMENT" :
-      return state+1;
-    case "DECREMENT" :
-      return state-1;
-      }
-}
-//store
-let store = createStore(counter)
-//to display our counter in the console window
-store.subscribe(() => console.log(store.getState())); //we are login the current state of our store
-//dispatcher
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
